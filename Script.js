c8c2bea8a988a7604f9854abf5992e7ee7711b58
Jocimar Rodrigues Chagas <JocimarRodrigues1337@gmail.com>
2022-10-21 04:41:56 -0300

    Version 0.5

---
+++ Script.js
@@ -0,0 +1,36 @@
+function jogar() {
+    if (document.getElementById("pedra").checked == false && document.getElementById("papel").checked == false && document.getElementById("tesoura").checked == false) {
+        window.alert ('[ERRO] Selecione uma opção!');
+    } else {
+        var sorteio = Math.floor(Math.random() * 3);
+        switch(sorteio){
+            case 0:
+                document.getElementById("pc").src="Imagens/pcpedra.png";
+                break;
+            case 1:
+                document.getElementById("pc").src="Imagens/pcpapel.png";
+                break;
+            case 2:
+                document.getElementById("pc").src="Imagens/pctesoura.png";
+                break;       
+        }
+        if ((document.getElementById("pedra").checked == true && sorteio == 0) ||
+         (document.getElementById("papel").checked == true && sorteio == 1) ||
+         (document.getElementById("tesoura").checked == true && sorteio == 2)) {
+            document.getElementById("placar").innerHTML = "EMPATE";
+         } else if ((document.getElementById("pedra").checked == true && sorteio == 2) ||
+         (document.getElementById("papel").checked == true && sorteio == 0) ||
+         (document.getElementById("tesoura").checked == true && sorteio == 1)) {
+            document.getElementById("placar").innerHTML = "Jogador Venceu!";              
+            } else {
+                document.getElementById("placar").innerHTML = "Computador Venceu!";
+            }
+    }
+
+}
+
+function resetar() {
+    document.getElementById("pc").src="Imagens/pc.png";
+    document.getElementById("placar").innerHTML="";
+
+}
